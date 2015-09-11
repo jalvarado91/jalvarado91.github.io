ceb4d61d3085503c2b5e5a5a90b8803f6323aba6
Juan Alvarado <jalvarado91@gmail.com>
2015-09-11 08:01:54 -0400

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', function() {
+gulp.task('styles', function() {
+	gulp.src('./scss/styles.scss')
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 version']
+		}))
+		.pipe(gulp.dest('./css'));
+});
 
+
+gulp.task('serve', ['styles'], function() {
+	browserSync({
+		server: {
+			baseDir: './'
+		}
+	});
+
+	gulp.watch('app/scss/.*scss', ['sass']);
 });
